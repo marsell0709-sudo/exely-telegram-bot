28068df6e73e22dd425fbcca7429e1c16c055c8e
marsell0709-sudo <marsell.0709@mail.ru>
2026-07-07 17:41:53 +0500

    Update search.py

--- app/handlers/search.py
+++ app/handlers/search.py
@@ -323,16 +323,16 @@ async def choose_guests(callback: CallbackQuery, state: FSMContext):
             media = [
                 InputMediaPhoto(media=image)
                 for image in images[:5]
-    ]
+           ]
 
             await callback.message.answer_media_group(media=media)
 
-            await callback.message.answer(
+      await callback.message.answer(
             text,
             parse_mode="HTML",
             reply_markup=keyboard,
             disable_web_page_preview=True,
-            )
+    )
 
     await callback.answer()
 
